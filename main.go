1e118f93fe22433b37d1957793d69c912bec7e2b
MATEUS SANTOS BISPO <mateusb.bispo@gmail.com>
2023-02-22 18:05:25 -0300

    Implementing interface

--- main.go
+++ main.go
@@ -5,10 +5,24 @@ import (
 	"fmt"
 )
 
+func PagarBoleto(conta verificarConta, valorDoBoleto float64) {
+	conta.Sacar(valorDoBoleto)
+}
+
+type verificarConta interface {
+	Sacar(valor float64) string
+}
+
 func main() {
 	contaDaBruna := contas.ContaPoupanca{}
-	contaDoMateus := contas.ContaCorrente{}
+	contaDaBruna.Depositar(100)
+	PagarBoleto(&contaDaBruna, 60)
 
-	fmt.Println(contaDaBruna)
-	fmt.Println(contaDoMateus)
+	fmt.Println(contaDaBruna.ObterSaldo())
+
+	contaDoMateus := contas.ContaCorrente{}
+	contaDoMateus.Depositar(500)
+	PagarBoleto(&contaDoMateus, 380)
+
+	fmt.Println(contaDoMateus.ObterSaldo())
 }
